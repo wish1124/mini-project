b0578e5bbae5f1b6125ed8803d761bc8da8b7b71
aibruter <jby8000@gmail.com>
2025-12-04 14:38:19 +0900

    backend

---
+++ backend/demo/src/test/java/com/kt_miniproject/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kt_miniproject.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
