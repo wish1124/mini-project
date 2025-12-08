51eea3bc92e77e82ee4a7792afcbc7a953331b0e
Lee Seyong <rred5899@gmail.com>
2025-12-08 11:37:05 +0900

    User Service login find Email Password

--- backend/demo/src/main/java/com/kt_miniproject/demo/service/UserService.java
+++ backend/demo/src/main/java/com/kt_miniproject/demo/service/UserService.java
@@ -1,5 +1,6 @@
 package com.kt_miniproject.demo.service;
 
+import com.kt_miniproject.demo.dto.user.LoginRequest;
 import com.kt_miniproject.demo.dto.user.UserCreateRequest;
 import com.kt_miniproject.demo.dto.user.UserResponse;
 
@@ -10,4 +11,8 @@ public interface UserService {
     UserResponse createUser(UserCreateRequest request);
 
     UserResponse getUserInfo(Long id);
+
+    UserResponse login(LoginRequest request); // 로그인
+    List<String> findEmail(String name);            // 이메일(ID) 찾기
+    String findPassword(String email, String name); // 비밀번호 찾기
 }
